fb1c509a902de6ebe556974b68de4743a297e252
Tikhon Jelvis <tikhon@jelv.is>
2014-02-19 22:22:01 -0800

    Finished the pretty printing code.

--- src/Untyped.hs
+++ src/Untyped.hs
@@ -3,21 +3,30 @@ module Untyped where
 
 import Text.Printf (printf)
 
+-- | A name can either be a local variable or a global
+--   definition. Local variables are annotated with their binding
+--   depth (ie deBruijn indexing).
 data Name = Global String
           | Local Int String
           deriving (Eq)
 
 instance Show Name where
-  show (Glboal name)  = name
+  show (Global name)  = name
   show (Local _ name) = name
 
--- | A term in the λ-calculus:
+-- | A term in the untyped λ-calculus:
 data Term = Var Name
           | Term :@: Term
           | Lam Name Term
           deriving (Eq)
 
 instance Show Term where
-  show (Var name)     = show name
-  show (e₁ :@: e₂)    = show e₁ ++ " " ++ show e₂
-  show (Lam arg body) = printf "λ%s.%s" (show arg) (show body)
+  show (Var name)       = show name
+  show (Lam arg body)   = printf "λ%s. %s" (show arg) (show body)
+  show (e₁ :@: e₂)      = printf "%s %s" (fn e₁) (arg e₂)
+    where fn λ@Lam{}      = printf "(%s)" $ show λ
+          fn e            = show e
+          arg app@(_:@:_) = printf "(%s)" $ show app
+          arg e           = fn e 
+
+
