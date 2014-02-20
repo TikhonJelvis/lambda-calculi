3b1d73d8772f0791d3486caef8cfb669613dbba2
Tikhon Jelvis <tikhon@jelv.is>
2014-02-20 00:31:02 -0800

    Wrote the parsing code and tests.

--- src/Untyped.hs
+++ src/Untyped.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE LambdaCase #-}
 
 -- | A very simple interpreter for the untyped λ-calculus.
-module Untyped where
+module Untyped (Globals, Locals, Term (..), Name (..), empty, eval, markLocals, parseTerm) where
 
-import qualified Data.Map    as Map
-import           Data.Map    (Map)
-import           Data.Maybe  (fromMaybe)
+import           Control.Applicative ((<*), (*>), (<*>), (<$>), (<$))
 
-import           Text.Printf (printf)
+import           Data.List           (elemIndex)
+import qualified Data.Map            as Map
+import           Data.Map            (Map)
+import           Data.Maybe          (fromMaybe)
+
+import qualified Text.Parsec         as P
+import           Text.Parsec         ((<|>), (<?>))
+import           Text.Parsec.String  (Parser)
+import           Text.Printf         (printf)
 
 -- | A name can either be a local variable or a global
 --   definition. Local variables are annotated with their binding
@@ -19,6 +25,16 @@ data Name = Global String
 instance Show Name where
   show (Global name)  = name
   show (Local _ name) = name
+
+-- | A valid variable name is made up of alphanumeric characters and
+--   underscores.
+-- 
+--   Variables get parsed as globals, with a separate pass doing all
+--   the bookkeeping for local variables.
+ident :: Parser Name
+ident = Global <$> P.many1 (P.alphaNum <|> P.char '_') <* P.spaces <?> "variable name"
+
+
 
 -- | A term in the untyped λ-calculus:
 data Term = Var Name
@@ -34,6 +50,42 @@ instance Show Term where
           fn e            = show e
           arg app@(_:@:_) = printf "(%s)" $ show app
           arg e           = fn e 
+
+symbol :: String -> Parser String
+symbol c    = P.string c <* P.spaces
+
+term, atom, variable, lambda, parens :: Parser Term
+
+-- | Parses a term, does not handle local variables.
+term =  application <|> atom
+  where application = foldl1 (:@:) <$> P.many1 atom
+
+atom =  lambda  
+    <|> variable
+    <|> parens
+
+variable  = Var <$> ident
+lambda    = λ *> (Lam <$> (ident <* symbol ".") <*> term)
+  where λ = symbol "λ" <|> symbol "\\"
+parens    = symbol "(" *> term <* symbol ")"
+
+-- | Goes through a term keeping track of scopes and annotates locally
+--   bound variables with their deBruijn index.
+markLocals :: Term -> Term
+markLocals = go []
+  where go scope = \case
+          var@(Var (Global name)) -> fromMaybe var $ local <$> elemIndex name scope
+            where local index = Var $ Local index name
+          var@Var{}     -> var
+          Lam name body -> Lam name $ go (show name : scope) body
+          e₁ :@: e₂     -> go scope e₁ :@: go scope e₂
+
+-- | Try to parse a λ-calculus term, returning either the result or a
+--   parse error.
+parseTerm :: String -> String -> Either P.ParseError Term
+parseTerm source str = markLocals <$> P.parse (P.spaces *> term <* P.eof) source str
+
+
 
 -- | The global environment maps global names to values. It's separate
 --   from locally bound names because those use deBruijn indexing.
@@ -59,5 +111,10 @@ eval globals = go []
             | index < length locals -> locals !! index
             | otherwise             -> var
           λ@Lam {}                  -> λ -- we do not evaluate under lambdas
-          (Lam _ body) :@: term     -> go (go locals term : locals) body
-          fn :@: term               -> go locals $ go locals fn :@: term
+          (Lam _ body) :@: expr     -> go (go locals expr : locals) body
+          fn :@: expr               -> go locals $ go locals fn :@: expr
+
+-- | Parses and then evaluate a λ-calculus term. Might return a parse
+--   error.
+run :: Globals -> String -> Either P.ParseError String
+run globals str = show . eval globals <$> parseTerm "" str
--- test/Test/Untyped.hs
+++ test/Test/Untyped.hs
@@ -8,7 +8,7 @@ import qualified Test.Tasty.QuickCheck as QC
 
 import           Untyped
 
-tests = testGroup "Untyped" [prettyPrinting]
+tests = testGroup "Untyped" [prettyPrinting, parsing]
 
 instance QC.Arbitrary Name where
   arbitrary = QC.oneof [Local <$> QC.arbitrary <*> QC.arbitrary, Global <$> QC.arbitrary]
@@ -54,3 +54,50 @@ prettyPrinting = testGroup "Pretty Printing"
           ]
         [x', y', f', foo', bar'] = Var <$> vars
         
+parsing = testGroup "Parsing"
+  [ testCase "variables" $ do
+       show (parseTerm "" "x")         @?= "Right x"
+       show (parseTerm "" "y")         @?= "Right y"
+       show (parseTerm "" "foo")       @?= "Right foo"
+       show (parseTerm "" "bar")       @?= "Right bar"
+       show (parseTerm "" "fooBar")    @?= "Right fooBar"
+       show (parseTerm "" "foo_bar")   @?= "Right foo_bar"
+       show (parseTerm "" "Foo_bar")   @?= "Right Foo_bar"
+       show (parseTerm "" "__Foo_bar") @?= "Right __Foo_bar"
+       show (parseTerm "" "__")        @?= "Right __"
+       show (parseTerm "" "   x")      @?= "Right x"
+       show (parseTerm "" "x   ")      @?= "Right x"
+       show (parseTerm "" "   x   ")   @?= "Right x"
+
+  , testCase "lambdas" $ do
+       show (parseTerm "" "(λ x.y)")          @?= "Right λx. y"
+       show (parseTerm "" "λ x.y")            @?= "Right λx. y"
+       show (parseTerm "" "(λ x.y) z")        @?= "Right (λx. y) z"
+       show (parseTerm "" "λ x.y z")          @?= "Right λx. y z"
+       show (parseTerm "" "z (λ x.y)")        @?= "Right z (λx. y)"
+       show (parseTerm "" "(λ x.y) (λ y. z)") @?= "Right (λx. y) (λy. z)"
+       show (parseTerm "" "λ x. λ y. z")      @?= "Right λx. λy. z"
+       show (parseTerm "" "λ x. (λ y. z)")    @?= "Right λx. λy. z"
+
+  , testCase "applications" $ do
+       show (parseTerm "" "f x")              @?= "Right f x"
+       show (parseTerm "" "f x y")            @?= "Right f x y"
+       show (parseTerm "" "(f x) y")          @?= "Right f x y"
+       show (parseTerm "" "f x y z")          @?= "Right f x y z"
+       show (parseTerm "" "(f x) y z")        @?= "Right f x y z"
+       show (parseTerm "" "(f x y) z")        @?= "Right f x y z"
+       show (parseTerm "" "((f x) y) z")      @?= "Right f x y z"
+       show (parseTerm "" "f (g y)")          @?= "Right f (g y)"
+       show (parseTerm "" "f (g y) x")        @?= "Right f (g y) x"
+       show (parseTerm "" "f x (g y)")        @?= "Right f x (g y)"
+       show (parseTerm "" "f (x g y)")        @?= "Right f (x g y)"
+       show (parseTerm "" "f λx .x")          @?= "Right f (λx. x)"
+       show (parseTerm "" "f (λx .x)")        @?= "Right f (λx. x)"
+       show (parseTerm "" "(λx .x) x")        @?= "Right (λx. x) x"
+       show (parseTerm "" "λx .x x")          @?= "Right λx. x x"
+       show (parseTerm "" "(λx .x) (λ y. y)") @?= "Right (λx. x) (λy. y)"
+       show (parseTerm "" "(λx .x) λ y. y")   @?= "Right (λx. x) (λy. y)"
+       show (parseTerm "" "λx .x (λ y. y)")   @?= "Right λx. x (λy. y)"
+       show (parseTerm "" "λx .x λ y. y")     @?= "Right λx. x (λy. y)"
+  ]
+
