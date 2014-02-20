25c4869125002a55e26799e1a8397830d57c68ce
Tikhon Jelvis <tikhon@jelv.is>
2014-02-19 22:46:55 -0800

    Wrote a simple eager eval function.

--- src/Untyped.hs
+++ src/Untyped.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE LambdaCase #-}
+
 -- | A very simple interpreter for the untyped λ-calculus.
 module Untyped where
 
-import Text.Printf (printf)
+import qualified Data.Map    as Map
+import           Data.Map    (Map)
+import           Data.Maybe  (fromMaybe)
+
+import           Text.Printf (printf)
 
 -- | A name can either be a local variable or a global
 --   definition. Local variables are annotated with their binding
@@ -29,4 +35,29 @@ instance Show Term where
           arg app@(_:@:_) = printf "(%s)" $ show app
           arg e           = fn e 
 
+-- | The global environment maps global names to values. It's separate
+--   from locally bound names because those use deBruijn indexing.
+type Globals = Map String Term
 
+-- | No names defined in global scope.
+empty :: Globals
+empty = Map.empty
+
+-- | We can just store our locally bound variables in a stack and
+--   index with !!
+type Locals = [Term]
+
+-- | Evaluate the term, using eager evaluation (for now).
+-- 
+--   Free variables are left unevaluated, so @foo@ would evaluate to
+--   itself.
+eval :: Globals -> Term -> Term
+eval globals = go []
+  where go locals = \case
+          var@(Var (Global name))   -> fromMaybe var $ Map.lookup name globals
+          var@(Var (Local index _))
+            | index < length locals -> locals !! index
+            | otherwise             -> var
+          λ@Lam {}                  -> λ -- we do not evaluate under lambdas
+          (Lam _ body) :@: term     -> go (go locals term : locals) body
+          fn :@: term               -> go locals $ go locals fn :@: term
